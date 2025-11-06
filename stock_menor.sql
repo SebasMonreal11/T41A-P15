5c0cfbc7f124a5b80ad7e89ee7c558696c860ab9
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-05 19:32:56 -0600

    Create stock_menor.sql

---
+++ stock_menor.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE FUNCTION productos_bajo_stock(minimo INT)
+RETURNS TABLE(id INT, nombre TEXT, stock INT) AS $$
+BEGIN
+    RETURN QUERY SELECT id, nombre, stock FROM productos WHERE stock < minimo;
+END;
+$$ LANGUAGE plpgsql;
