43e15f594628c76dca80b5af28c025d56dd68b24
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-05 19:48:04 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO empleados (id, nombre, departamento_id, edad) VALUES
+(1, 'Ana Torres', 10, 25),
+(2, 'Luis Gómez', 10, 32),
+(3, 'Marta Ruiz', 20, 17),
+(4, 'Carlos Díaz', 30, 45),
+(5, 'Lucía Pérez', 20, 19);
+
