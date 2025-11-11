dcde4cd7e8c08660b805804baee506c6d067020f
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-10 19:45:33 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -4,7 +4,7 @@ VALUES (10, 'TI'), (20, 'Ventas'), (30, 'Recursos Humanos');
 INSERT INTO empleados (id, nombre, departamento_id, edad) VALUES
 (1, 'Ana Torres', 10, 25),
 (2, 'Luis Gómez', 10, 32),
-(3, 'Marta Ruiz', 20, 17),
+(3, 'María Ruiz', 10, 17),
 (4, 'Carlos Díaz', 30, 45),
 (5, 'Lucía Pérez', 20, 19);
 
